d17d80547b5ab7255d38b19b18fa98487db3a66b
Matt Glaman <nmd.matt@gmail.com>
2019-11-04 10:00:51 -0600

    Iniital merge of Cart API

---
+++ modules/commerce_cart_api/src/Resource/CartCouponAddResource.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace Drupal\commerce_cart_api\Resource;
+
+use Drupal\commerce_api\EntityResourceShim;
+use Drupal\commerce_order\Entity\OrderInterface;
+use Drupal\commerce_promotion\CouponStorageInterface;
+use Drupal\commerce_promotion\Entity\CouponInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Render\RenderContext;
+use Drupal\Core\Render\RendererInterface;
+use Drupal\jsonapi\Access\EntityAccessChecker;
+use Drupal\jsonapi\Entity\EntityValidationTrait;
+use Drupal\jsonapi\JsonApiResource\ResourceIdentifier;
+use Drupal\jsonapi\JsonApiResource\ResourceObject;
+use Drupal\jsonapi\JsonApiResource\ResourceObjectData;
+use Drupal\jsonapi\ResourceType\ResourceTypeRepositoryInterface;
+use Drupal\jsonapi_resources\Resource\EntityResourceBase;
+use Drupal\jsonapi_resources\ResourceResponseFactory;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\UnprocessableEntityHttpException;
+
+/**
+ * Cart add coupon resource.
+ */
+final class CartCouponAddResource extends EntityResourceBase {
+
+  use EntityValidationTrait;
+
+  /**
+   * The JSON:API controller shim.
+   *
+   * @var \Drupal\commerce_api\EntityResourceShim
+   */
+  protected $inner;
+
+  /**
+   * The renderer.
+   *
+   * @var \Drupal\Core\Render\Renderer|object|null
+   */
+  private $renderer;
+
+  /**
+   * Constructs a new CartCouponAddResource object.
+   *
+   * @param \Drupal\jsonapi_resources\ResourceResponseFactory $resource_response_factory
+   *   The resource response factory.
+   * @param \Drupal\jsonapi\ResourceType\ResourceTypeRepositoryInterface $resource_type_repository
+   *   The resource type repository.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   * @param \Drupal\jsonapi\Access\EntityAccessChecker $entity_access_checker
+   *   The entity access checker.
+   * @param \Drupal\commerce_api\EntityResourceShim $jsonapi_controller
+   *   The JSON:API controller shim.
+   * @param \Drupal\Core\Render\RendererInterface $renderer
+   *   The renderer.
+   */
+  public function __construct(ResourceResponseFactory $resource_response_factory, ResourceTypeRepositoryInterface $resource_type_repository, EntityTypeManagerInterface $entity_type_manager, EntityAccessChecker $entity_access_checker, EntityResourceShim $jsonapi_controller, RendererInterface $renderer) {
+    parent::__construct($resource_response_factory, $resource_type_repository, $entity_type_manager, $entity_access_checker);
+    $this->inner = $jsonapi_controller;
+    $this->renderer = $renderer;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('jsonapi_resources.resource_response_factory'),
+      $container->get('jsonapi.resource_type.repository'),
+      $container->get('entity_type.manager'),
+      $container->get('jsonapi_resources.entity_access_checker'),
+      $container->get('commerce_api.jsonapi_controller_shim'),
+      $container->get('renderer')
+    );
+  }
+
+  /**
+   * Processes the request.
+   *
+   * @param \Symfony\Component\HttpFoundation\Request $request
+   *   The request.
+   * @param \Drupal\commerce_order\Entity\OrderInterface $commerce_order
+   *   The order.
+   *
+   * @return \Drupal\jsonapi\ResourceResponse
+   *   The response.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   * @throws \Drupal\Core\Entity\EntityStorageException
+   * @throws \Drupal\Core\TypedData\Exception\ReadOnlyException
+   */
+  public function process(Request $request, OrderInterface $commerce_order) {
+    $resource_type = $this->resourceTypeRepository->get($commerce_order->getEntityTypeId(), $commerce_order->bundle());
+    $internal_relationship_field_name = $resource_type->getInternalName('coupons');
+    /* @var \Drupal\jsonapi\JsonApiResource\ResourceIdentifier[] $resource_identifiers */
+    $resource_identifiers = $this->inner->deserialize($resource_type, $request, ResourceIdentifier::class, 'coupons');
+
+    $context = new RenderContext();
+    /** @var \Drupal\commerce_promotion\Entity\CouponInterface[] $coupons */
+    $coupons = $this->renderer->executeInRenderContext($context, function () use ($resource_identifiers) {
+      $coupons = [];
+      $coupon_storage = $this->entityTypeManager->getStorage('commerce_promotion_coupon');
+      assert($coupon_storage instanceof CouponStorageInterface);
+      foreach ($resource_identifiers as $resource_identifier) {
+        $coupon = $coupon_storage->loadEnabledByCode($resource_identifier->getId());
+        if (!$coupon instanceof CouponInterface) {
+          throw new UnprocessableEntityHttpException(sprintf('%s is not a valid coupon code.', $resource_identifier->getId()));
+        }
+        $coupons[] = $coupon;
+      }
+      return $coupons;
+    });
+
+    /* @var \Drupal\Core\Field\EntityReferenceFieldItemListInterface $field_list */
+    $field_list = $commerce_order->{$internal_relationship_field_name};
+    $field_list->setValue($coupons);
+    self::validate($commerce_order);
+    $commerce_order->save();
+
+    return $this->inner->getRelationship(
+      $this->resourceTypeRepository->get($commerce_order->getEntityTypeId(), $commerce_order->bundle()),
+      $commerce_order,
+      'coupons',
+      $request
+    );
+  }
+
+}
---
+++ modules/commerce_cart_api/src/Routing/Routes.php
@@ -0,0 +1,291 @@
+<?php
+
+namespace Drupal\commerce_cart_api\Routing;
+
+use Drupal\commerce\PurchasableEntityInterface;
+use Drupal\commerce_cart_api\Resource\CartAddResource;
+use Drupal\commerce_cart_api\Resource\CartCanonicalResource;
+use Drupal\commerce_cart_api\Resource\CartClearResource;
+use Drupal\commerce_cart_api\Resource\CartCollectionResource;
+use Drupal\commerce_cart_api\Resource\CartCouponAddResource;
+use Drupal\commerce_cart_api\Resource\CartRemoveItemResource;
+use Drupal\commerce_cart_api\Resource\CartUpdateItemResource;
+use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\jsonapi\ParamConverter\ResourceTypeConverter;
+use Drupal\jsonapi\ResourceType\ResourceType;
+use Drupal\jsonapi\ResourceType\ResourceTypeRepositoryInterface;
+use Drupal\jsonapi\Routing\Routes as JsonapiRoutes;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\RouteCollection;
+
+class Routes implements ContainerInjectionInterface {
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * The JSON:API resource type repository.
+   *
+   * @var \Drupal\jsonapi\ResourceType\ResourceTypeRepositoryInterface
+   */
+  protected $resourceTypeRepository;
+
+  /**
+   * List of providers.
+   *
+   * @var string[]
+   */
+  protected $providerIds;
+
+  /**
+   * The JSON:API base path.
+   *
+   * @var string
+   */
+  protected $jsonApiBasePath;
+
+  /**
+   * Instantiates a Routes object.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   * @param \Drupal\jsonapi\ResourceType\ResourceTypeRepositoryInterface $resource_type_repository
+   *   The JSON:API resource type repository.
+   * @param string[] $authentication_providers
+   *   The authentication providers, keyed by ID.
+   * @param string $jsonapi_base_path
+   *   The JSON:API base path.
+   */
+  public function __construct(EntityTypeManagerInterface $entity_type_manager, ResourceTypeRepositoryInterface $resource_type_repository, array $authentication_providers, $jsonapi_base_path) {
+    $this->entityTypeManager = $entity_type_manager;
+    $this->resourceTypeRepository = $resource_type_repository;
+    $this->providerIds = array_keys($authentication_providers);
+    $this->jsonApiBasePath = $jsonapi_base_path;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('entity_type.manager'),
+      $container->get('jsonapi.resource_type.repository'),
+      $container->getParameter('authentication_providers'),
+      $container->getParameter('jsonapi.base_path')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function routes() {
+    $routes = new RouteCollection();
+
+    $routes->add('commerce_cart_api.jsonapi.cart_collection', $this->cartsCollection());
+    $routes->add('commerce_cart_api.jsonapi.cart_canonical', $this->cartCanonical());
+    $routes->add('commerce_cart_api.jsonapi.cart_clear', $this->cartClear());
+    $routes->add('commerce_cart_api.jsonapi.cart_add', $this->cartAdd());
+    $routes->add('commerce_cart_api.jsonapi.cart_remove_item', $this->cartRemoveItem());
+    $routes->add('commerce_cart_api.jsonapi.cart_update_item', $this->cartUpdateItem());
+
+    if ($this->entityTypeManager->hasDefinition('commerce_promotion_coupon')) {
+      $routes->add('commerce_cart_api.jsonapi.cart_coupon_add', $this->cartCouponAdd());
+    }
+
+    // All routes must pass _cart_api access check.
+    $routes->addRequirements([
+      '_access' => 'TRUE',
+      '_cart_api' => 'TRUE',
+    ]);
+
+    // Set a resource type so entity UUID parameter conversion works.
+    // This also will upcast the resource type and allow for OpenAPI documentation.
+    $routes->addDefaults([JsonapiRoutes::RESOURCE_TYPE_KEY => 'commerce_order--virtual']);
+
+    return $routes;
+  }
+
+  /**
+   * The cart collection resource route.
+   *
+   * @return \Symfony\Component\Routing\Route
+   *   The route.
+   */
+  protected function cartsCollection() {
+    $order_resource_types = array_filter($this->resourceTypeRepository->all(), static function (ResourceType $resource_type) {
+      return $resource_type->getEntityTypeId() === 'commerce_order';
+    });
+    $order_resource_types = array_map(static function (ResourceType $resource_type) {
+      return $resource_type->getTypeName();
+    }, $order_resource_types);
+
+    $route = new Route('/cart');
+    $route->addDefaults([
+      '_jsonapi_resource' => CartCollectionResource::class,
+      '_jsonapi_resource_types' => $order_resource_types,
+    ]);
+    return $route;
+  }
+
+  /**
+   * The cart canonical resource route.
+   *
+   * @return \Symfony\Component\Routing\Route
+   *   The route.
+   */
+  protected function cartCanonical() {
+    $order_resource_types = array_filter($this->resourceTypeRepository->all(), static function (ResourceType $resource_type) {
+      return $resource_type->getEntityTypeId() === 'commerce_order';
+    });
+    $order_resource_types = array_map(static function (ResourceType $resource_type) {
+      return $resource_type->getTypeName();
+    }, $order_resource_types);
+
+    $route = new Route('/cart/{commerce_order}');
+    $route->addDefaults([
+      '_jsonapi_resource' => CartCanonicalResource::class,
+      '_jsonapi_resource_types' => $order_resource_types,
+    ]);
+    $parameters = $route->getOption('parameters') ?: [];
+    $parameters['commerce_order']['type'] = 'entity:commerce_order';
+    $route->setOption('parameters', $parameters);
+    return $route;
+  }
+
+  /**
+   * The cart clear resource route.
+   *
+   * @return \Symfony\Component\Routing\Route
+   *   The route.
+   */
+  protected function cartClear() {
+    $route = new Route('/cart/{commerce_order}');
+    $route->addDefaults([
+      '_jsonapi_resource' => CartClearResource::class,
+    ]);
+    $route->setMethods(['DELETE']);
+    $parameters = $route->getOption('parameters') ?: [];
+    $parameters['commerce_order']['type'] = 'entity:commerce_order';
+    $route->setOption('parameters', $parameters);
+    return $route;
+  }
+
+  /**
+   * The cart add resource route.
+   *
+   * @return \Symfony\Component\Routing\Route
+   *   The route.
+   */
+  protected function cartAdd() {
+    $purchasable_entity_resource_types = array_filter($this->resourceTypeRepository->all(), function (ResourceType $resource_type) {
+      $entity_type = $this->entityTypeManager->getDefinition($resource_type->getEntityTypeId());
+      return $entity_type->entityClassImplements(PurchasableEntityInterface::class);
+    });
+    $purchasable_entity_resource_types = array_map(static function (ResourceType $resource_type) {
+      return $resource_type->getTypeName();
+    }, $purchasable_entity_resource_types);
+
+    $order_item_resource_types = array_filter($this->resourceTypeRepository->all(), function (ResourceType $resource_type) {
+      return $resource_type->getEntityTypeId() === 'commerce_order_item';
+    });
+    $order_item_resource_types = array_map(static function (ResourceType $resource_type) {
+      return $resource_type->getTypeName();
+    }, $order_item_resource_types);
+
+    $route = new Route('/cart/add');
+    $route->addDefaults([
+      '_jsonapi_resource' => CartAddResource::class,
+      '_purchasable_entity_resource_types' => $purchasable_entity_resource_types,
+      '_jsonapi_resource_types' => $order_item_resource_types,
+    ]);
+    $route->setMethods(['POST']);
+
+    return $route;
+  }
+
+  /**
+   * The cart remove item resource route.
+   *
+   * @return \Symfony\Component\Routing\Route
+   *   The route.
+   */
+  protected function cartRemoveItem() {
+    $order_item_resource_types = array_filter($this->resourceTypeRepository->all(), function (ResourceType $resource_type) {
+      return $resource_type->getEntityTypeId() === 'commerce_order_item';
+    });
+    $order_item_resource_types = array_map(static function (ResourceType $resource_type) {
+      return $resource_type->getTypeName();
+    }, $order_item_resource_types);
+
+    $route = new Route('/cart/{commerce_order}/items');
+    $route->addDefaults([
+      '_jsonapi_resource' => CartRemoveItemResource::class,
+      '_order_item_resource_types' => $order_item_resource_types,
+    ]);
+    $route->setMethods(['DELETE']);
+    $parameters = $route->getOption('parameters') ?: [];
+    $parameters['commerce_order']['type'] = 'entity:commerce_order';
+    $route->setOption('parameters', $parameters);
+    return $route;
+  }
+
+  /**
+   * The cart update item resource route.
+   *
+   * @return \Symfony\Component\Routing\Route
+   *   The route.
+   */
+  protected function cartUpdateItem() {
+    $order_item_resource_types = array_filter($this->resourceTypeRepository->all(), function (ResourceType $resource_type) {
+      return $resource_type->getEntityTypeId() === 'commerce_order_item';
+    });
+    $order_item_resource_types = array_map(static function (ResourceType $resource_type) {
+      return $resource_type->getTypeName();
+    }, $order_item_resource_types);
+
+    $route = new Route('/cart/{commerce_order}/items/{commerce_order_item}');
+    $route->addDefaults([
+      '_jsonapi_resource' => CartUpdateItemResource::class,
+      '_jsonapi_resource_types' => $order_item_resource_types,
+    ]);
+    $route->setMethods(['PATCH']);
+    $parameters = $route->getOption('parameters') ?: [];
+    $parameters['commerce_order']['type'] = 'entity:commerce_order';
+    $parameters['commerce_order_item']['type'] = 'entity:commerce_order_item';
+    $route->setOption('parameters', $parameters);
+    return $route;
+  }
+
+  /**
+   * The cart coupon add resource route.
+   *
+   * @return \Symfony\Component\Routing\Route
+   *   The route.
+   */
+  protected function cartCouponAdd() {
+    $coupon_resource_types = array_filter($this->resourceTypeRepository->all(), function (ResourceType $resource_type) {
+      return $resource_type->getEntityTypeId() === 'commerce_promotion_coupon';
+    });
+    $coupon_resource_types = array_map(static function (ResourceType $resource_type) {
+      return $resource_type->getTypeName();
+    }, $coupon_resource_types);
+
+    $route = new Route('/cart/{commerce_order}/coupons');
+    $route->setMethods(['PATCH']);
+    $route->addDefaults([
+      '_jsonapi_resource' => CartCouponAddResource::class,
+      '_jsonapi_resource_types' => $coupon_resource_types,
+    ]);
+    $parameters = $route->getOption('parameters') ?: [];
+    $parameters['commerce_order']['type'] = 'entity:commerce_order';
+    $route->setOption('parameters', $parameters);
+    return $route;
+  }
+
+}
