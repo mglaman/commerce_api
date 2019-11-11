26ffa62aa18d50f85fffaa903a32ea87215a99a4
Matt Glaman <nmd.matt@gmail.com>
2019-11-11 13:50:53 -0600

    WIP for Search API support

---
+++ src/Resource/SearchApi/IndexResource.php
@@ -0,0 +1,132 @@
+<?php declare(strict_types = 1);
+
+namespace Drupal\commerce_api\Resource\SearchApi;
+
+use Drupal\commerce_api\Utils;
+use Drupal\Core\Cache\CacheableMetadata;
+use Drupal\Core\Http\Exception\CacheableBadRequestHttpException;
+use Drupal\Core\Url;
+use Drupal\jsonapi\JsonApiResource\Link;
+use Drupal\jsonapi\JsonApiResource\LinkCollection;
+use Drupal\jsonapi\Query\OffsetPage;
+use Drupal\jsonapi\ResourceResponse;
+use Drupal\jsonapi_resources\Resource\EntityResourceBase;
+use Drupal\search_api\IndexInterface;
+use Drupal\search_api\Item\ItemInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+final class IndexResource extends EntityResourceBase {
+
+  public function process(Request $request, IndexInterface $index): ResourceResponse {
+    $cacheability = new CacheableMetadata();
+    $query = $index->query();
+
+    // Ensure that different pages will be cached separately.
+    $cacheability->addCacheContexts(['url.query_args:page']);
+    // Derive any pagination options from the query params or use defaults.
+    $pagination = $request->query->has('page')
+      ? OffsetPage::createFromQueryParameter($request->query->get('page'))
+      : new OffsetPage(OffsetPage::DEFAULT_OFFSET, OffsetPage::SIZE_MAX);
+    $query->range($pagination->getOffset(), $pagination->getSize());
+
+    $results = $query->execute();
+    $result_entities = array_map(function (ItemInterface $item) {
+      return $item->getOriginalObject()->getValue();
+    }, \iterator_to_array($results));
+    $primary_data = $this->createCollectionDataFromEntities(array_values($result_entities));
+
+    $pager_links = new LinkCollection([]);
+    $total_count = (int) $results->getResultCount();
+    $size = $pagination->getSize();
+    $has_next_page = $total_count > $size;
+    $offset = $pagination->getOffset();
+    if ($pagination->getSize() <= 0) {
+      throw new CacheableBadRequestHttpException($cacheability, sprintf('The page size needs to be a positive integer.'));
+    }
+    $query = (array) $request->query->getIterator();
+
+    // Check if this is not the last page.
+    if ($has_next_page) {
+      $next_url = Utils::getRequestLink($request, static::getPagerQueries('next', $offset, $size, $query));
+      $pager_links = $pager_links->withLink('next', new Link(new CacheableMetadata(), $next_url, 'next'));
+
+      if (!empty($total_count)) {
+        $last_url = Utils::getRequestLink($request, static::getPagerQueries('last', $offset, $size, $query, $total_count));
+        $pager_links = $pager_links->withLink('last', new Link(new CacheableMetadata(), $last_url, 'last'));
+      }
+    }
+    // Check if this is not the first page.
+    if ($offset > 0) {
+      $first_url = Utils::getRequestLink($request, static::getPagerQueries('first', $offset, $size, $query));
+      $pager_links = $pager_links->withLink('first', new Link(new CacheableMetadata(), $first_url, 'first'));
+      $prev_url = Utils::getRequestLink($request, static::getPagerQueries('prev', $offset, $size, $query));
+      $pager_links = $pager_links->withLink('prev', new Link(new CacheableMetadata(), $prev_url, 'prev'));
+    }
+
+    $response = $this->createJsonapiResponse($primary_data, $request, 200, [], $pager_links);
+    $response->addCacheableDependency($cacheability);
+    return $response;
+  }
+
+  /**
+   * Get the query param array.
+   *
+   * @param string $link_id
+   *   The name of the pagination link requested.
+   * @param int $offset
+   *   The starting index.
+   * @param int $size
+   *   The pagination page size.
+   * @param array $query
+   *   The query parameters.
+   * @param int $total
+   *   The total size of the collection.
+   *
+   * @return array
+   *   The pagination query param array.
+   */
+  protected static function getPagerQueries($link_id, $offset, $size, array $query = [], $total = 0) {
+    $extra_query = [];
+    switch ($link_id) {
+      case 'next':
+        $extra_query = [
+          'page' => [
+            'offset' => $offset + $size,
+            'limit' => $size,
+          ],
+        ];
+        break;
+
+      case 'first':
+        $extra_query = [
+          'page' => [
+            'offset' => 0,
+            'limit' => $size,
+          ],
+        ];
+        break;
+
+      case 'last':
+        if ($total) {
+          $extra_query = [
+            'page' => [
+              'offset' => (ceil($total / $size) - 1) * $size,
+              'limit' => $size,
+            ],
+          ];
+        }
+        break;
+
+      case 'prev':
+        $extra_query = [
+          'page' => [
+            'offset' => max($offset - $size, 0),
+            'limit' => $size,
+          ],
+        ];
+        break;
+    }
+    return array_merge($query, $extra_query);
+  }
+
+}
---
+++ src/Utils.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types = 1);
+
+namespace Drupal\commerce_api;
+
+use Drupal\Core\Url;
+use Symfony\Component\HttpFoundation\Request;
+
+final class Utils {
+
+  /**
+   * Get the full URL for a given request object.
+   *
+   * @param \Symfony\Component\HttpFoundation\Request $request
+   *   The request object.
+   * @param array|null $query
+   *   The query parameters to use. Leave it empty to get the query from the
+   *   request object.
+   *
+   * @return \Drupal\Core\Url
+   *   The full URL.
+   */
+  public static function getRequestLink(Request $request, $query = NULL) {
+    if ($query === NULL) {
+      return Url::fromUri($request->getUri());
+    }
+
+    $uri_without_query_string = $request->getSchemeAndHttpHost() . $request->getBaseUrl() . $request->getPathInfo();
+    return Url::fromUri($uri_without_query_string)->setOption('query', $query);
+  }
+
+}
