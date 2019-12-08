1cdbd2a07c0e2941b3ea113073477f082c318955
Matt Glaman <nmd.matt@gmail.com>
2019-12-08 11:40:46 -0600

    Provide a resource for the current store

---
+++ src/Resource/CurrentStoreResource.php
@@ -0,0 +1,19 @@
+<?php declare(strict_types = 1);
+
+namespace Drupal\commerce_api\Resource;
+
+use Drupal\commerce_store\CurrentStoreInterface;
+use Drupal\jsonapi_resources\Resource\EntityResourceBase;
+use Symfony\Component\HttpFoundation\Request;
+
+final class CurrentStoreResource extends EntityResourceBase {
+
+    public function process(Request $request) {
+        $current_store = \Drupal::service('commerce_store.current_store');
+        assert($current_store instanceof CurrentStoreInterface);
+        $store = $current_store->getStore();
+        $data = $this->createIndividualDataFromEntity($store);
+        return $this->createJsonapiResponse($data, $request, 200);
+    }
+
+}
