7ab01309491381010253e8057c4a731a0ccdcff9
Matt Glaman <nmd.matt@gmail.com>
2019-12-16 10:28:19 -0600

    Ensure order_total includes formatted properties

--- src/Plugin/Field/ComputedOrderTotal.php
+++ src/Plugin/Field/ComputedOrderTotal.php
@@ -2,8 +2,10 @@
 
 namespace Drupal\commerce_api\Plugin\Field;
 
+use CommerceGuys\Intl\Formatter\CurrencyFormatterInterface;
 use Drupal\commerce_order\Adjustment;
 use Drupal\commerce_order\Entity\OrderInterface;
+use Drupal\commerce_order\OrderTotalSummaryInterface;
 use Drupal\commerce_price\Price;
 use Drupal\Core\Field\FieldItemList;
 use Drupal\Core\TypedData\ComputedItemListTrait;
@@ -12,13 +14,23 @@ final class ComputedOrderTotal extends FieldItemList {
   use ComputedItemListTrait;
 
   /**
+   * @var \CommerceGuys\Intl\Formatter\CurrencyFormatterInterface
+   */
+  private $currencyFormatter;
+
+  /**
    * {@inheritdoc}
    */
   protected function computeValue() {
     $order = $this->getEntity();
     assert($order instanceof OrderInterface);
+    $currency_formatter = \Drupal::service('commerce_price.currency_formatter');
+    assert($currency_formatter instanceof CurrencyFormatterInterface);
+    $this->currencyFormatter = $currency_formatter;
+
     $summary = \Drupal::getContainer()->get('commerce_order.order_total_summary');
-    $totals = array_map([static::class, 'valueObjectsToArray'], $summary->buildTotals($order));
+    assert($summary instanceof OrderTotalSummaryInterface);
+    $totals = array_map([$this, 'valueObjectsToArray'], $summary->buildTotals($order));
     $this->list[0] = $this->createItem(0, $totals);
   }
 
@@ -31,15 +43,17 @@ final class ComputedOrderTotal extends FieldItemList {
    * @return array
    *   The value, as an array.
    */
-  protected static function valueObjectsToArray($value) {
+  protected function valueObjectsToArray($value) {
     if ($value instanceof Price) {
-      return $value->toArray();
+      return $value->toArray() + [
+        'formatted' => $this->currencyFormatter->format($value->getNumber(), $value->getCurrencyCode()),
+      ];
     }
     if ($value instanceof Adjustment) {
       return $value->toArray();
     }
     if (is_array($value)) {
-      return array_map([static::class, 'valueObjectsToArray'], $value);
+      return array_map([$this, 'valueObjectsToArray'], $value);
     }
     return $value;
   }
