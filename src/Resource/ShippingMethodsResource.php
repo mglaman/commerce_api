916f19e0dea88f9b8b6e6f120abba1908050df75
Jonathan Sacksick <jonathan.sacksick@gmail.com>
2020-02-05 16:27:59 +0200

    Remove the delivery terms.

--- src/Resource/ShippingMethodsResource.php
+++ src/Resource/ShippingMethodsResource.php
@@ -89,7 +89,6 @@ final class ShippingMethodsResource extends ResourceBase implements ContainerInj
             'serviceId' => $service->getId(),
             'amount' => $rate->getAmount()->toArray(),
             'deliveryDate' => $delivery_date ? $delivery_date->format(DateTimeItemInterface::DATETIME_STORAGE_FORMAT) : NULL,
-            'terms' => $rate->getDeliveryTerms(),
           ],
           // @todo link template to provide PATCH data :?
           new LinkCollection([])
